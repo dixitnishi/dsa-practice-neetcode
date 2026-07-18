7db42be7dc16f4cb91a0e95ec8e3fb2ce516051c
Nishi Dixit <97115447+dixitnishi@users.noreply.github.com>
2026-07-18 14:42:38 +0530

    Add: k-closest-points-to-origin - submission-1

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+        // using min heap
+        // store the min distance and index of the points in min heap
+        // calculate the distance
+        PriorityQueue<Pair> pq = new PriorityQueue<>((p1,p2) -> Double.compare(p1.distance,p2.distance));
+
+        for(int i = 0;i<points.length;i++){
+            int x = points[i][0];
+            int y = points[i][1];
+            double distance = Math.sqrt(Math.pow(Math.abs(x - 0),2) + Math.pow(Math.abs(y - 0),2));
+            pq.offer(new Pair(i,distance));
+        }
+        int ans[][] = new int[k][2];
+        for(int i = 0;i<k;i++){
+            Pair p = pq.poll();
+            ans[i] = points[p.idx];
+        }
+        return ans;
+        
+    }
+}
+
+class Pair{
+    int idx;
+    double distance;
+
+    public Pair(int idx,double distance){
+        this.idx = idx;
+        this.distance = distance;
+    }
+}
